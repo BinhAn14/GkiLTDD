e40c72b975cea5cc48500ff0fea4ad36669bac8c
BinhAn14 <an100277@donga.edu.vn>
2024-11-12 16:01:39 +0700

    ip

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const tripRoutes = require('./routes/tripRoutes');
 const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 
 // Kết nối cơ sở dữ liệu MongoDB
-mongoose.connect('mongodb://localhost:27017/tourService', {
+mongoose.connect('mongodb+srv://an100277:AeeReISLciRp5Aqf@binhan.awftk.mongodb.net/tourService?retryWrites=true&w=majority&appName=binhan', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
