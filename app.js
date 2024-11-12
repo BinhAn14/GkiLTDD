87808e66a8f6e7c99dd30c319535af3a180733af
BinhAn14 <an100277@donga.edu.vn>
2024-11-12 17:12:32 +0700

    ip

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ const userRoutes = require('./routes/userRoutes');
 require('dotenv').config(); // Load biến môi trường từ .env
 
 const app = express();
-
 app.use(express.json());
 
 // Kết nối cơ sở dữ liệu MongoDB
 const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://an100277:AeeReISLciRp5Aqf@binhan.awftk.mongodb.net/tourService?retryWrites=true&w=majority';
 
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 30000, // Tăng thời gian chờ kết nối
+        socketTimeoutMS: 30000, // Tăng thời gian chờ socket
+    })
     .then(() => console.log('Kết nối cơ sở dữ liệu thành công'))
     .catch((error) => console.error('Lỗi kết nối cơ sở dữ liệu:', error));
 
