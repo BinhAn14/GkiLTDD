8436adda88fd82d2c3edb833a5f31adaf1474363
BinhAn14 <an100277@donga.edu.vn>
2024-11-12 17:04:41 +0700

    ip

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const tripRoutes = require('./routes/tripRoutes');
 const userRoutes = require('./routes/userRoutes');
-require('dotenv').config();
+require('dotenv').config(); // Load biến môi trường từ .env
 
 const app = express();
 
 app.use(express.json());
 
 // Kết nối cơ sở dữ liệu MongoDB
-mongoose.connect('mongodb+srv://an100277:AeeReISLciRp5Aqf@binhan.awftk.mongodb.net/tourService?retryWrites=true&w=majority&appName=binhan', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://an100277:AeeReISLciRp5Aqf@binhan.awftk.mongodb.net/tourService?retryWrites=true&w=majority';
+
+mongoose.connect(mongoURI)
     .then(() => console.log('Kết nối cơ sở dữ liệu thành công'))
     .catch((error) => console.error('Lỗi kết nối cơ sở dữ liệu:', error));
 
@@ -21,7 +20,7 @@ app.use('/api/trips', tripRoutes);
 app.use('/api/users', userRoutes);
 
 // Khởi động server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server đang chạy trên cổng ${PORT}`);
 });
